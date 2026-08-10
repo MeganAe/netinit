856d518ea2802aa609784fbb946e668aa5ae2d1b
Metoushela Walker <168326500+MeganAe@users.noreply.github.com>
2026-08-11 02:44:17 +0300

    Delete api directory

--- api/index.js
+++
@@ -1,4 +0,0 @@
-// Point d'entrée utilisé par Vercel : une seule fonction serverless qui
-// exécute l'application Express. Le fichier vercel.json redirige toutes
-// les requêtes /api/* vers cette fonction (voir sa configuration).
-module.exports = require("../server/app");
